ae87fcc261a3210dbd5fd67c2170f6e2e225d42e
Melanie Wilson <melaniemwilson1@gmail.com>
2023-04-07 21:51:33 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+// function findMatching (driversArray, name) {
+//     const newDriversArray = []
+//     for (const driver of driversArray){
+//         if(driver.toUpperCase() === name.toUpperCase()) {
+//             newDriversArray.push(driver)
+//         }
+//     }
+//     return newDriversArray;
+// }
+
+function findMatching(driversArray, name) {
+    return driversArray.filter(driver => driver.toUpperCase() === name.toUpperCase())
+}
+
+function fuzzyMatch(driversArray, beginningName) {
+    return driversArray.filter(driver => driver.toUpperCase().substring(0, beginningName.length) === beginningName.toUpperCase())
+}
+
+function matchName(driversObject, targetName) {
+    return driversObject.filter(driver => driver.name.toUpperCase() === targetName.toUpperCase())
+}
